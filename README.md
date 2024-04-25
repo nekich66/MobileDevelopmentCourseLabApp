c24afea666f5c757e53fef007694d5e6e78e8548
nekich66 <160615470+nekich66@users.noreply.github.com>
2024-04-25 21:26:36 +0500

    Task1

--- README.md
+++ README.md
@@ -70,4 +70,5 @@
 14. Этой реакцией станет запуск Intent для отправки данных. Он запустит стандартный экран шеринга https://developer.android.com/training/sharing/send
 15. Повторите пп. 14-16 из первой практики
 
+task1 ![img.png](img.png)
 
--- app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobiledevelopmentcourselabapp
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -33,5 +34,38 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView?.setupWithNavController(navController)
+
+        Log.d("Lifecycles", "${this::class.simpleName} - onCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycles", "${this::class.simpleName} - onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycles", "${this::class.simpleName} - onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycles", "${this::class.simpleName} - onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycles", "${this::class.simpleName} - onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycles", "${this::class.simpleName} - onDestroy")
+
     }
 }
